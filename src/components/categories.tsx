d4f8684fe6dae0a6683343935bbdca587ab3e323
JFaithTrust <jahongirsolijoniy@gmail.com>
2024-03-06 21:48:58 +0500

    create any components

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -1,5 +1,4 @@
 import { CategoryCard } from "./parts"
-import { Button } from "./ui/button"
 
 const Categories = () => {
   return (
@@ -8,11 +7,6 @@ const Categories = () => {
         <h1 className="text-center font-roboto text-3xl font-semibold">Get work done in over 72 different <span className="text-darkblue">Categories</span></h1>
         <div className="grid grid-cols-6 gap-3">
           <CategoryCard />
-        </div>
-        <div className="flex items-center justify-center">
-          <Button variant={'outline'} className="px-5 py-3 font-roboto text-lg rounded-xl">
-            See More...
-          </Button>
         </div>
       </div>
     </div>
--- src/pages/jobs/jobs.tsx
+++ src/pages/jobs/jobs.tsx
@@ -1,6 +1,15 @@
 import { getAllCategory } from "@/api/fetchCategory";
 import { getDistrictByRegionId } from "@/api/fetchDistrict";
-import { getCountOfAllJobs, getJobsByPagination } from "@/api/fetchJobs";
+import {
+  getCountOfAllJobs,
+  getJobByCatergoryId,
+  getJobsByDistrictId,
+  getJobsByGender,
+  getJobsByMaxAndMinAge,
+  getJobsByMaxAndMinSalary,
+  getJobsByPagination,
+  getJobsByRegionId,
+} from "@/api/fetchJobs";
 import { getAllRegion } from "@/api/fetchRegion";
 import { JobCard, Partners } from "@/components/parts";
 import { Button } from "@/components/ui/button";
@@ -23,6 +32,8 @@ import { cn } from "@/lib/utils";
 import { Category, District, Jobs, Region } from "@/types";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { useState, useEffect } from "react";
+import { toast } from "sonner";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState<Jobs[]>([]);
@@ -39,8 +50,16 @@ const Jobs = () => {
   const [valuer, setValuer] = useState("");
   const [opend, setOpend] = useState(false);
   const [valued, setValued] = useState("");
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
+  // max and min salary states
+  const [minSalary, setMinSalary] = useState(0);
+  const [maxSalary, setMaxSalary] = useState(0);
+  // min and max age states
+  const [minAge, setMinAge] = useState(0);
+  const [maxAge, setMaxAge] = useState(0);
+  // gender state
+  const [currentGender, setCunrrentGender] = useState("Tanlanmagan");
+
   const usersPerPage = 9;
   // const lastUsersIndex = currentPage * usersPerPage;
   // const firstUserIndex = lastUsersIndex - usersPerPage;
@@ -73,21 +92,113 @@ const Jobs = () => {
   }, [currentPage]);
 
   useEffect(() => {
+    if (valuec) {
+      const selectedCategoryId =
+        allCategory.find(
+          (category) => category.title.toLocaleLowerCase() === valuec
+        )?.id || "";
+      getJobByCatergoryId(selectedCategoryId, currentPage, usersPerPage).then(
+        (jobs) => setJobs(jobs)
+      );
+    } else {
+      getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
+        setJobs(jobs)
+      );
+    }
+  }, [valuec]);
+
+  useEffect(() => {
     if (valuer) {
+      const selectedRegionId =
+        regions.find((region) => region.name.toLocaleLowerCase() === valuer)
+          ?.id || "";
+      getJobsByRegionId(selectedRegionId, currentPage, usersPerPage)
+        .then((jobs) => setJobs(jobs))
+        .catch((err) => {
+          return toast.error(err);
+        });
       getDistrictByRegionId(
         regions.find((region) => region.name.toLocaleLowerCase() === valuer)
           ?.id || ""
       ).then((districts) => setDistrict(districts));
+    } else {
+      getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
+        setJobs(jobs)
+      );
     }
   }, [valuer]);
 
   useEffect(() => {
-    console.log(valuec);
-  }, [valuec]);
+    if (valued) {
+      const selectedDistrictId =
+        district.find((d) => d.name.toLocaleLowerCase() === valued)?.id || "";
+      getJobsByDistrictId(selectedDistrictId, currentPage, usersPerPage).then(
+        (jobs) => setJobs(jobs)
+      );
+    } else {
+      const selectedRegionId =
+        regions.find((region) => region.name.toLocaleLowerCase() === valuer)
+          ?.id || "";
+      getJobsByRegionId(selectedRegionId, currentPage, usersPerPage)
+        .then((jobs) => setJobs(jobs))
+        .catch((err) => {
+          return toast.error(err);
+        });
+    }
+  }, [valued]);
 
-  // useEffect(() => {
-  //   getAllJobsByQueryParams(currentPage).then((data) => setJobs(data));
-  // }, [currentPage])
+  // submit min and max salary
+  const handleSubmitMinMaxSalary = () => {
+    if (minSalary > maxSalary) {
+      toast.error("Maosh chegarasi noto'g'ri kiritildi!");
+      return;
+    }
+    getJobsByMaxAndMinSalary(
+      minSalary,
+      maxSalary,
+      currentPage,
+      usersPerPage
+    ).then((data) => setJobs(data));
+  };
+
+  // submit min and max age
+  const handleSubmitMinMaxAge = () => {
+    if (minAge > maxAge) {
+      toast.error("Yosh chegarasi noto'g'ri kiritildi!");
+      return;
+    }
+    getJobsByMaxAndMinAge(minAge, maxAge, currentPage, usersPerPage).then(
+      (data) => setJobs(data)
+    );
+  };
+
+  // submit by gender
+  const handleSubmitGender = (gender: string) => {
+    if (gender === currentGender) {
+      setCunrrentGender("Tanlanmagan");
+      return getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
+        setJobs(jobs)
+      );
+    }
+    if (gender === "Erkak") {
+      setCunrrentGender("Erkak");
+      getJobsByGender("Erkak", currentPage, usersPerPage).then((data) =>
+        setJobs(data)
+      );
+    } else {
+      setCunrrentGender("Ayol");
+      getJobsByGender("Ayol", currentPage, usersPerPage).then((data) =>
+        setJobs(data)
+      );
+    }
+  };
+
+  // reset
+  // const handleReset = () => {
+  //   getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
+  //     setJobs(jobs)
+  //   );
+  // };
 
   return (
     <div className="flex flex-col gap-y-8 mt-8">
@@ -105,37 +216,59 @@ const Jobs = () => {
           </div>
           <div className="grid grid-cols-6 gap-x-4">
             <div className="col-span-1 flex flex-col gap-y-4">
-              {/* Cost */}
+              {/* Salary */}
               <div>
                 <Label className="text-base font-normal text-darkindigo">
                   Summa
                 </Label>
                 <div className="grid grid-cols-3 gap-x-2">
                   <div className="flex flex-col gap-y-2 col-span-2">
-                    <Input placeholder="Min" />
-                    <Input placeholder="Max" />
+                    <Input
+                      placeholder="Min"
+                      type="number"
+                      onChange={(e) => setMinSalary(parseInt(e.target.value))}
+                    />
+                    <Input
+                      placeholder="Max"
+                      type="number"
+                      onChange={(e) => setMaxSalary(parseInt(e.target.value))}
+                    />
                   </div>
                   <div className="h-full col-span-1 flex items-end">
-                    <Button className="h-fit items-end px-3 py-1 rounded-2xl">
+                    <Button
+                      className="h-fit items-end px-3 py-1 rounded-2xl"
+                      onClick={handleSubmitMinMaxSalary}
+                    >
                       Go
                     </Button>
                   </div>
                 </div>
               </div>
-              {/* Salary */}
+              {/* Age */}
               <div>
                 <Label className="text-base font-normal text-darkindigo">
                   Yosh chegarasi
                 </Label>
                 <div className="grid grid-cols-3 gap-x-2 content-center">
                   <div className="col-span-1">
-                    <Input placeholder="Min" />
+                    <Input
+                      placeholder="Min"
+                      type="number"
+                      onChange={(e) => setMinAge(parseInt(e.target.value))}
+                    />
                   </div>
                   <div className="col-span-1">
-                    <Input placeholder="Max" />
+                    <Input
+                      placeholder="Max"
+                      type="number"
+                      onChange={(e) => setMaxAge(parseInt(e.target.value))}
+                    />
                   </div>
                   <div className="h-full col-span-1 flex items-end">
-                    <Button className="h-fit items-end px-3 py-1 rounded-2xl">
+                    <Button
+                      className="h-fit items-end px-3 py-1 rounded-2xl"
+                      onClick={handleSubmitMinMaxAge}
+                    >
                       Go
                     </Button>
                   </div>
@@ -147,22 +280,26 @@ const Jobs = () => {
                   Jins
                 </Label>
                 <div className="grid grid-cols-2 gap-x-2">
-                  <div className="col-span-1">
-                    <Button
-                      variant={"active-outline"}
-                      className="items-end px-3 py-1 rounded-xl"
+                  <ToggleGroup type="single">
+                    <ToggleGroupItem
+                      value="bold"
+                      aria-label="Toggle bold"
+                      className="col-span-1 px-3 py-1 rounded-xl"
+                      variant={"outline"}
+                      onClick={() => handleSubmitGender("Erkak")}
                     >
                       Erkak
-                    </Button>
-                  </div>
-                  <div className="col-span-1">
-                    <Button
-                      variant={"active-outline"}
-                      className="items-end px-3 py-1 rounded-xl"
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                      value="italic"
+                      aria-label="Toggle italic"
+                      className="col-span-1 px-3 py-1 rounded-xl"
+                      variant={"outline"}
+                      onClick={() => handleSubmitGender("Ayol")}
                     >
                       Ayol
-                    </Button>
-                  </div>
+                    </ToggleGroupItem>
+                  </ToggleGroup>
                 </div>
               </div>
               {/* Category Combobox */}
