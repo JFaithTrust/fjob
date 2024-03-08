e7d4832efba3fe3d5e0ae78485a6353f16923f24
Abdurasul <abdurasulabduraimov22@gmail.com>
2024-03-08 14:24:29 +0500

    filter removed from jobs page

--- src/pages/jobs/jobs.tsx
+++ src/pages/jobs/jobs.tsx
@@ -1,55 +1,22 @@
-import { getAllCategory } from "@/api/fetchCategory";
-import { getDistrictByRegionId } from "@/api/fetchDistrict";
-import {
-  getCountOfAllJobs,
-  getJobByCatergoryId,
-  getJobsByDistrictId,
-  getJobsByGender,
-  // getJobsByMaxAndMinAge,
-  // getJobsByMaxAndMinSalary,
-  getJobsByPagination,
-  getJobsByRegionId,
-} from "@/api/fetchJobs";
-import { getAllRegion } from "@/api/fetchRegion";
+// import { getDistrictByRegionId } from "@/api/fetchDistrict";
+import { getCountOfAllJobs, getJobsByPagination, } from "@/api/fetchJobs";
+// import { getAllRegion } from "@/api/fetchRegion";
 import { JobCard, Partners } from "@/components/parts";
-import { Button } from "@/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "@/components/ui/command";
 import CustomPagination from "@/components/ui/custom-pagination";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { cn } from "@/lib/utils";
-import { Category, District, Jobs, Region } from "@/types";
-import { Check, ChevronsUpDown } from "lucide-react";
-import { useState, useEffect } from "react";
-import { toast } from "sonner";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Jobs } from "@/types";
+import { useEffect, useState } from "react";
+// import { toast } from "sonner";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState<Jobs[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   // part states of jobs
-  const [allCategory, setAllCategory] = useState<Category[]>([]);
-  const [regions, setRegions] = useState<Region[]>([]);
-  const [district, setDistrict] = useState<District[]>([]);
+  // const [allCategory, setAllCategory] = useState<Category[]>([]);
+  // const [regions, setRegions] = useState<Region[]>([]);
+  // const [district, setDistrict] = useState<District[]>([]);
   // combobox states
-  const [openc, setOpenc] = useState(false);
-  const [valuec, setValuec] = useState("");
-  const [openr, setOpenr] = useState(false);
-  const [valuer, setValuer] = useState("");
-  const [opend, setOpend] = useState(false);
-  const [valued, setValued] = useState("");
   const [count, setCount] = useState(0);
   // max and min salary states
   // const [minSalary, setMinSalary] = useState(0);
@@ -58,7 +25,6 @@ const Jobs = () => {
   // const [minAge, setMinAge] = useState(0);
   // const [maxAge, setMaxAge] = useState(0);
   // gender state
-  const [currentGender, setCunrrentGender] = useState("Tanlanmagan");
 
   const usersPerPage = 9;
   // const lastUsersIndex = currentPage * usersPerPage;
@@ -87,65 +53,65 @@ const Jobs = () => {
     getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
       setJobs(jobs)
     );
-    getAllCategory().then((categories) => setAllCategory(categories));
-    getAllRegion().then((regions) => setRegions(regions));
+    // getAllCategory().then((categories) => setAllCategory(categories));
+    // getAllRegion().then((regions) => setRegions(regions));
   }, [currentPage]);
 
-  useEffect(() => {
-    if (valuec) {
-      const selectedCategoryId =
-        allCategory.find(
-          (category) => category.title.toLocaleLowerCase() === valuec
-        )?.id || "";
-      getJobByCatergoryId(selectedCategoryId, currentPage, usersPerPage).then(
-        (jobs) => setJobs(jobs)
-      );
-    } else {
-      getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
-        setJobs(jobs)
-      );
-    }
-  }, [valuec]);
-
-  useEffect(() => {
-    if (valuer) {
-      const selectedRegionId =
-        regions.find((region) => region.name.toLocaleLowerCase() === valuer)
-          ?.id || "";
-      getJobsByRegionId(selectedRegionId, currentPage, usersPerPage)
-        .then((jobs) => setJobs(jobs))
-        .catch((err) => {
-          return toast.error(err);
-        });
-      getDistrictByRegionId(
-        regions.find((region) => region.name.toLocaleLowerCase() === valuer)
-          ?.id || ""
-      ).then((districts) => setDistrict(districts));
-    } else {
-      getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
-        setJobs(jobs)
-      );
-    }
-  }, [valuer]);
-
-  useEffect(() => {
-    if (valued) {
-      const selectedDistrictId =
-        district.find((d) => d.name.toLocaleLowerCase() === valued)?.id || "";
-      getJobsByDistrictId(selectedDistrictId, currentPage, usersPerPage).then(
-        (jobs) => setJobs(jobs)
-      );
-    } else {
-      const selectedRegionId =
-        regions.find((region) => region.name.toLocaleLowerCase() === valuer)
-          ?.id || "";
-      getJobsByRegionId(selectedRegionId, currentPage, usersPerPage)
-        .then((jobs) => setJobs(jobs))
-        .catch((err) => {
-          return toast.error(err);
-        });
-    }
-  }, [valued]);
+  // useEffect(() => {
+  //   if (valuec) {
+  //     const selectedCategoryId =
+  //       allCategory.find(
+  //         (category) => category.title.toLocaleLowerCase() === valuec
+  //       )?.id || "";
+  //     getJobByCatergoryId(selectedCategoryId, currentPage, usersPerPage).then(
+  //       (jobs) => setJobs(jobs)
+  //     );
+  //   } else {
+  //     getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
+  //       setJobs(jobs)
+  //     );
+  //   }
+  // }, [valuec]);
+  //
+  // useEffect(() => {
+  //   if (valuer) {
+  //     const selectedRegionId =
+  //       regions.find((region) => region.name.toLocaleLowerCase() === valuer)
+  //         ?.id || "";
+  //     getJobsByRegionId(selectedRegionId, currentPage, usersPerPage)
+  //       .then((jobs) => setJobs(jobs))
+  //       .catch((err) => {
+  //         return toast.error(err);
+  //       });
+  //     getDistrictByRegionId(
+  //       regions.find((region) => region.name.toLocaleLowerCase() === valuer)
+  //         ?.id || ""
+  //     ).then((districts) => setDistrict(districts));
+  //   } else {
+  //     getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
+  //       setJobs(jobs)
+  //     );
+  //   }
+  // }, [valuer]);
+  //
+  // useEffect(() => {
+  //   if (valued) {
+  //     const selectedDistrictId =
+  //       district.find((d) => d.name.toLocaleLowerCase() === valued)?.id || "";
+  //     getJobsByDistrictId(selectedDistrictId, currentPage, usersPerPage).then(
+  //       (jobs) => setJobs(jobs)
+  //     );
+  //   } else {
+  //     const selectedRegionId =
+  //       regions.find((region) => region.name.toLocaleLowerCase() === valuer)
+  //         ?.id || "";
+  //     getJobsByRegionId(selectedRegionId, currentPage, usersPerPage)
+  //       .then((jobs) => setJobs(jobs))
+  //       .catch((err) => {
+  //         return toast.error(err);
+  //       });
+  //   }
+  // }, [valued]);
 
   // submit min and max salary
   // const handleSubmitMinMaxSalary = () => {
@@ -173,25 +139,7 @@ const Jobs = () => {
   // };
 
   // submit by gender
-  const handleSubmitGender = (gender: string) => {
-    if (gender === currentGender) {
-      setCunrrentGender("Tanlanmagan");
-      return getJobsByPagination(currentPage, usersPerPage).then((jobs) =>
-        setJobs(jobs)
-      );
-    }
-    if (gender === "Erkak") {
-      setCunrrentGender("Erkak");
-      getJobsByGender("Erkak", currentPage, usersPerPage).then((data) =>
-        setJobs(data)
-      );
-    } else {
-      setCunrrentGender("Ayol");
-      getJobsByGender("Ayol", currentPage, usersPerPage).then((data) =>
-        setJobs(data)
-      );
-    }
-  };
+
 
   // reset
   // const handleReset = () => {
@@ -215,233 +163,7 @@ const Jobs = () => {
             />
           </div>
           <div className="grid grid-cols-6 gap-x-4">
-            <div className="col-span-1 flex flex-col gap-y-4">
-              {/* Salary */}
-              <div>
-                <Label className="text-base font-normal text-darkindigo">
-                  Summa
-                </Label>
-                <div className="grid grid-cols-3 gap-x-2">
-                  <div className="flex flex-col gap-y-2 col-span-2">
-                    <Input
-                      placeholder="Min"
-                      type="number"
-                      // onChange={(e) => setMinSalary(parseInt(e.target.value))}
-                    />
-                    <Input
-                      placeholder="Max"
-                      type="number"
-                      // onChange={(e) => setMaxSalary(parseInt(e.target.value))}
-                    />
-                  </div>
-                </div>
-              </div>
-              {/* Age */}
-              <div>
-                <Label className="text-base font-normal text-darkindigo">
-                  Yosh chegarasi
-                </Label>
-                <div className="grid grid-cols-3 gap-x-2 content-center">
-                  <div className="col-span-1">
-                    <Input
-                      placeholder="Min"
-                      type="number"
-                      // onChange={(e) => setMinAge(parseInt(e.target.value))}
-                    />
-                  </div>
-                  <div className="col-span-1">
-                    <Input
-                      placeholder="Max"
-                      type="number"
-                      // onChange={(e) => setMaxAge(parseInt(e.target.value))}
-                    />
-                  </div>
-                </div>
-              </div>
-              {/* Gender */}
-              <div>
-                <Label className="text-base font-normal text-darkindigo">
-                  Jins
-                </Label>
-                <div className="grid grid-cols-2 gap-x-2">
-                  <ToggleGroup type="single">
-                    <ToggleGroupItem
-                      value="bold"
-                      aria-label="Toggle bold"
-                      className="col-span-1 px-3 py-1 rounded-xl"
-                      variant={"outline"}
-                      onClick={() => handleSubmitGender("Erkak")}
-                    >
-                      Erkak
-                    </ToggleGroupItem>
-                    <ToggleGroupItem
-                      value="italic"
-                      aria-label="Toggle italic"
-                      className="col-span-1 px-3 py-1 rounded-xl"
-                      variant={"outline"}
-                      onClick={() => handleSubmitGender("Ayol")}
-                    >
-                      Ayol
-                    </ToggleGroupItem>
-                  </ToggleGroup>
-                </div>
-              </div>
-              {/* Category Combobox */}
-              <div>
-                <Popover open={openc} onOpenChange={setOpenc}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={openc}
-                      className="w-[200px] justify-between bg-white hover:bg-white text-darkindigo hover:text-darkindigo p-2 rounded-lg"
-                    >
-                      {valuec
-                        ? allCategory.find(
-                            (category) =>
-                              category.title.toLocaleLowerCase() === valuec
-                          )?.title
-                        : "Kategoriya tanlang..."}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput placeholder="Search framework..." />
-                      <CommandEmpty>No framework found.</CommandEmpty>
-                      <CommandGroup>
-                        {allCategory.map((category) => (
-                          <CommandItem
-                            key={category.id}
-                            value={category.title}
-                            onSelect={(currentValue) => {
-                              setValuec(
-                                currentValue === valuec ? "" : currentValue
-                              );
-                              setOpenc(false);
-                            }}
-                          >
-                            <Check
-                              className={cn(
-                                "mr-2 h-4 w-4",
-                                valuec === category.title.toLocaleLowerCase()
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                            {category.title}
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
-              </div>
-              {/* Region Combobox */}
-              <div>
-                <Popover open={openr} onOpenChange={setOpenr}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={openr}
-                      className="w-[200px] justify-between bg-white hover:bg-white text-darkindigo hover:text-darkindigo p-2 rounded-lg"
-                    >
-                      <span className="truncate">
-                        {valuer
-                          ? regions.find(
-                              (region) =>
-                                region.name.toLocaleLowerCase() === valuer
-                            )?.name
-                          : "Viloyat tanlang..."}
-                      </span>
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput placeholder="Search framework..." />
-                      <CommandEmpty>No framework found.</CommandEmpty>
-                      <CommandGroup>
-                        {regions.map((region) => (
-                          <CommandItem
-                            key={region.id}
-                            value={region.name}
-                            onSelect={(currentValue) => {
-                              setValuer(
-                                currentValue === valuer ? "" : currentValue
-                              );
-                              setOpenr(false);
-                            }}
-                          >
-                            <Check
-                              className={cn(
-                                "mr-2 h-4 w-4",
-                                valuer === region.name.toLocaleLowerCase()
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                            {region.name}
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
-              </div>
-              {/* District Combobox */}
-              <div>
-                <Popover open={opend} onOpenChange={setOpend}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={opend}
-                      disabled={!valuer}
-                      className="w-[200px] justify-between bg-white hover:bg-white text-darkindigo hover:text-darkindigo p-2 rounded-lg"
-                    >
-                      {valued
-                        ? district.find(
-                            (d) => d.name.toLocaleLowerCase() === valued
-                          )?.name || "Tuman tanlang..."
-                        : "Tuman tanlang..."}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput placeholder="Search framework..." />
-                      <CommandEmpty>No framework found.</CommandEmpty>
-                      <CommandGroup>
-                        {district.map((d) => (
-                          <CommandItem
-                            key={d.id}
-                            value={d.name}
-                            onSelect={(currentValue) => {
-                              setValued(
-                                currentValue === valued ? "" : currentValue
-                              );
-                              setOpend(false);
-                            }}
-                          >
-                            <Check
-                              className={cn(
-                                "mr-2 h-4 w-4",
-                                valued === d.name.toLocaleLowerCase()
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                            {d.name}
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
-              </div>
-            </div>
+
             <div className="flex flex-col col-span-5">
               <div className="grid grid-cols-3 gap-3 w-full">
                 {jobs
