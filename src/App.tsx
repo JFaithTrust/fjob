7aeae64818eb6bfa55dfb5fb95ef83a537af9cd6
JFaithTrust <jahongirsolijoniy@gmail.com>
2024-03-29 15:51:48 +0500

    fixed: bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Home,
-  JobDetail,
-  Jobs,
-  Layout,
-  Workers,
-} from "./pages";
+import {Route, Routes} from "react-router-dom";
+
+import {About, Contact, Home, JobDetail, Jobs, Layout, Workers,} from "./pages";
 import WorkerDetail from "./pages/workers/worker-detail";
-import { Toaster } from "@/components/ui/sonner"
+import {Toaster} from "@/components/ui/sonner"
+import {SpeedInsights} from "@vercel/speed-insights/next";
 
 function App() {
   return (
     <div className="h-full">
-      <Toaster richColors />
+      <Toaster richColors/>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="jobs" element={<Jobs />} />
-          <Route path="job-detail/:jobId" element={<JobDetail />} />
-          <Route path="workers" element={<Workers />} />
-          <Route path="worker-detail/:workerId" element={<WorkerDetail />} />
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="jobs" element={<Jobs/>}/>
+          <Route path="job-detail/:jobId" element={<JobDetail/>}/>
+          <Route path="workers" element={<Workers/>}/>
+          <Route path="worker-detail/:workerId" element={<WorkerDetail/>}/>
         </Route>
       </Routes>
+      <SpeedInsights/>
     </div>
   );
 }
